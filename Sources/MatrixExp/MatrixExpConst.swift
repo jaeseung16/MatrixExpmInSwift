f061c97d31c363fb07c663d9e592ad60107253e8
Jae-Seung Lee <jaeseung@gmail.com>
2020-10-12 10:58:20 -0400

    Add thirteen to PadeApproximantOrder
    Fix padeCoefficients(for:) and theta(for:)

--- Sources/MatrixExp/MatrixExpConst.swift
+++ Sources/MatrixExp/MatrixExpConst.swift
@@ -15,6 +15,7 @@ struct MatrixExpConst {
         case five = 5
         case seven = 7
         case nine = 9
+        case thirteen = 13
     }
     
     static func padeCoefficients(for order: Int) -> Vector<Double>? {
@@ -30,9 +31,11 @@ struct MatrixExpConst {
         case .five:
             coeff = [30240.0, 15120.0, 3360.0, 420.0, 30.0, 1.0]
         case .seven:
+            coeff = [17297280.0, 8648640.0, 1995840.0, 277200.0, 25200.0, 1512.0, 56.0, 1.0]
+        case .nine:
             coeff = [17643225600.0, 8821612800.0, 2075673600.0, 302702400.0, 30270240.0,
                      2162160.0, 110880.0, 3960.0, 90.0, 1.0]
-        case .nine:
+        case .thirteen:
             coeff = [64764752532480000.0, 32382376266240000.0, 7771770303897600.0,
                      1187353796428800.0,  129060195264000.0,   10559470521600.0,
                      670442572800.0,      33522128640.0,       1323241920.0,
@@ -62,6 +65,8 @@ struct MatrixExpConst {
         case .seven:
             theta = 9.504178996162932e-001
         case .nine:
+            theta = 2.097847961257068e+000
+        case .thirteen:
             theta = 5.371920351148152e+000
         }
         
